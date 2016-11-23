12822f299aa2149f204ff6a624f6f15aebd8e844
Phiphou <phiphou@phiphou.com>
2016-11-23 02:37:43 +0100

    Move template in an externnal file

--- src/app/components/Hello.vue
+++ src/app/components/Hello.vue
@@ -1,31 +1,5 @@
-<template lang="pug">
-  .hello
-    a.refresh(href='/', @click.prevent='reload()') Refresh
-    .container
-      ul
-        li
-          input#g_all(type='radio', name='s', v-model='gender', value='all')
-          label#g_all(for='g_all') All
-          .check
-        li
-          input#g_male(type='radio', v-model='gender', value='male')
-          label#g_male(for='g_male') Male
-          .check
-        li
-          input#g_female(type='radio', name='s', v-model='gender', value='female')
-          label#g_female(for='g_female') Female
-          .check
-    ul.bob
-      li(v-for='person in filteredPeople')
-        .item
-          img.item_avatar(:src='person.picture.thumbnail')
-          .item_label
-            | {{person.name.first | capitalize}} {{person.name.last.toUpperCase()}}
-</template>
-
+<template lang="pug" src="./Hello.tpl.pug"></template>
 <script>
-// require('./style.sass')
-
 import Vue from 'vue'
 
 Vue.filter('capitalize', value => value.charAt(0).toUpperCase() + value.substr(1))
