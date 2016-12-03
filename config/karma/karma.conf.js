7a2b95160da19c9259ca21f7237752018153cd3f
Phiphou <phiphou@phiphou.com>
2016-12-04 00:53:37 +0100

    some cleaning

--- config/karma/karma.conf.js
+++ config/karma/karma.conf.js
@@ -1,8 +1,19 @@
 const pkg = require('../../package.json')
 module.exports = config => {
+  // http://karma-runner.github.io/1.0/config/configuration-file.html
   config.set({
     autoWatch: false,
+    browserDisconnectTimeout: 2000,
+    browserNoActivityTimeout: 10000,
+    browserConsoleLogOptions: {
+      level: 'debug',
+      format: '%b %T: %m',
+      terminal: true
+    },
     browsers: process.env.CIRCLE_ENV ? ['PhantomJS'] : ['Chrome'],
+    captureTimeout: 60000,
+    colors: true,
+    concurrency: Infinity,
     coverageReporter: {
       dir: '../../reports/coverage',
       reporters: process.env.CIRCLE_ENV
@@ -18,14 +29,26 @@ module.exports = config => {
         return browser.toLowerCase().split(/[ /-]/)[0]
       },
       watermarks: {
-        statements: [50, 99],
-        functions: [50, 99],
-        branches: [50, 99],
-        lines: [50, 99]
+        statements: [50, 90],
+        functions: [50, 90],
+        branches: [50, 90],
+        lines: [50, 90]
       }
     },
+    failOnEmptyTestSuite: true,
     frameworks: ['mocha', 'chai'],
     files: ['./karma.entry.js'],
+    htmlReporter: {
+      outputFile: '../../reports/unit/unit_tests_report.html',
+      pageTitle: pkg.name + ' - Unit Tests',
+      subPageTitle: pkg.description,
+      groupSuites: true,
+      useCompactStyle: false,
+      useLegacyStyle: false
+    },
+    junitReporter: {
+      outputFile: '../../../reports/unit/test-results.xml'
+    },
     logLevel: config.LOG_INFO,
     mochaReporter: {
       colors: {
@@ -57,17 +80,7 @@ module.exports = config => {
     ],
     preprocessors: {'./karma.entry.js': ['webpack']},
     reporters: ['mocha', 'html', 'junit', 'coverage'],
-    htmlReporter: {
-      outputFile: '../../reports/unit/unit_tests_report.html',
-      pageTitle: pkg.name + ' - Unit Tests',
-      subPageTitle: pkg.description,
-      groupSuites: true,
-      useCompactStyle: false,
-      useLegacyStyle: false
-    },
-    junitReporter: {
-      outputFile: '../../../reports/unit/test-results.xml'
-    },
+    reportSlowerThan: 0,
     singleRun: true,
     webpack: require('../webpack/webpack.test'),
     webpackServer: {noInfo: true}
